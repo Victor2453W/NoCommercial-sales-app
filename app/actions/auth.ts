b68507e6eb5fa1bba6f1f1a5e773d2570e5c71ff
возращение Гобнык <ruscobectb228@gmail.com>
2025-06-16 18:44:24 +0500

    start creating authentication. Fixed bugs

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,26 +1,26 @@
-import { SignupFormSchema, FormState } from '@/app/lib/definitions'
-import bcrypt from 'bcryptjs'
-import db from 'postgres'
+// import { SignupFormSchema, FormState } from '@/app/lib/definitions'
+// import bcrypt from 'bcryptjs'
+// import db from 'postgres'
 
-export async function signup(state: FormState, formData: FormData) {
-  // Validate form fields
-  const validatedFields = SignupFormSchema.safeParse({
-    name: formData.get('name'),
-    email: formData.get('email'),
-    password: formData.get('password'),
-  })
+// export async function signup(state: FormState, formData: FormData) {
+//   // Validate form fields
+//   const validatedFields = SignupFormSchema.safeParse({
+//     name: formData.get('name'),
+//     email: formData.get('email'),
+//     password: formData.get('password'),
+//   })
  
-  // If any form fields are invalid, return early
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-    }
-  }
+//   // If any form fields are invalid, return early
+//   if (!validatedFields.success) {
+//     return {
+//       errors: validatedFields.error.flatten().fieldErrors,
+//     }
+//   }
  
-  // 2. Prepare data for insertion into database
-  const { name, email, password } = validatedFields.data
-  // e.g. Hash the user's password before storing it
-  const hashedPassword = await bcrypt.hash(password, 10)
+//   // 2. Prepare data for insertion into database
+//   const { name, email, password } = validatedFields.data
+//   // e.g. Hash the user's password before storing it
+//   const hashedPassword = await bcrypt.hash(password, 10)
  
   // 3. Insert the user into the database or call an Auth Library's API
   // const data = await db
@@ -43,4 +43,4 @@ export async function signup(state: FormState, formData: FormData) {
   // TODO:
   // 4. Create user session
   // 5. Redirect user
-}
+// }
